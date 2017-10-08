082a9b327a607cc8ea908fa9ecc7b314f627611c
cat-plevak <catherine.plevak@colorado.edu>
2017-10-08 15:30:52 -0600

    merge conflict eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+ "extends": [
+    'airbnb'
+  ],
+  rules: {
+    "semi": "off",
+    "prefer-const": "off",
+    "no-implicit-globals": "off",
+    "strict": "off",
+    "quotes": "off",
+    "no-unused-vars": "off",
+    "no-else-return": "off",
+    "no-multiple-empty-lines": "off"
+  }
+};
