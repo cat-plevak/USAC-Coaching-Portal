b20d1a90f10a613f207d9bff19dc07720196b925
Jonny Hork <JonnyHork@gmail.com>
2017-10-09 15:28:53 -0600

    creates tables threads, working on populating table

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,7 +1,26 @@
 $(document).ready(() => {
   console.log('document ready!')
 
+  // if (document.location.href.match(/certified$/)) {
 
+  $.getJSON('/api/coaches/certified').then(data => {
+    console.log("List of coaches", data)
+    let tbody = $('#certifiedList tbody')
+
+    data.forEach((coach) => {
+      tbody.append($(`<tr>
+          <td>${coach.lastName}</td>
+          <td>${coach.firstName}</td>
+          <td>${coach.teamName}</td>
+          <td>${coach.cprExpDate}</td>
+          <td>${coach.faExpDate}</td>
+          <td>${coach.ssExpDate}</td>
+          <td>${coach.usacMembership}</td>
+          <td>${coach.isCertified}</td>
+        </tr>`))
+    })
+  })
+  // }
 
 
 
