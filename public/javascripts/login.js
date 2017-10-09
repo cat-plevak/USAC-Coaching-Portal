0190a69fbfc60a88494b17331c6eca965045e8a8
Brianna Greene <brianna.greene.designs@gmail.com>
2017-10-09 16:42:25 -0600

    updates

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -3,16 +3,22 @@ $(document).ready(function(){
   $('#login-button').click((e) => {
     e.preventDefault()
 
-    let email = $('#login-email').val()
+    let email = $('#login-email').val().trim()
     let password = $('#login-pass').val()
 
     console.log(email, password);
 
-    knex('users')
-      .select('*')
-      .then((data) => {
-        console.log(data);
-      })
+    if (!email) {
+        return next(boom.create('Email must not be blank', 404))
+      }
+
+    if (email.indexOf('@') < 0) {
+      return next(boom.create('Email must be valid', 404))
+    }
+
+    if (!password || password.length < 8) {
+      return next(boom.create('Password must be at least 8 characters', 404))
+    }
 
 
   })
