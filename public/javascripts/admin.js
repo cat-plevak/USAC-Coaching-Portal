df3b38fdcdbc172a57c44f9e1bbd14e6d12c9012
Jonny Hork <JonnyHork@gmail.com>
2017-10-09 16:02:42 -0600

    both tables populate

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,14 +1,14 @@
 $(document).ready(() => {
   console.log('document ready!')
 
-  // if (document.location.href.match(/certified$/)) {
+  if (document.location.href.match(/certified$/)) {
 
-  $.getJSON("/api/coaches/certified").then(data => {
-    console.log("List of coaches", data)
-    let tbody = $('#certifiedList tbody')
+    $.getJSON("/api/coaches/certified").then(data => {
+      console.log("List of coaches", data)
+      let tbody = $('#certifiedList tbody')
 
-    data.forEach((coach) => {
-      tbody.append($(`<tr>
+      data.forEach((coach) => {
+        tbody.append($(`<tr>
           <td>${coach.lastName}</td>
           <td>${coach.firstName}</td>
           <td>${coach.teamName}</td>
@@ -18,9 +18,30 @@ $(document).ready(() => {
           <td>${coach.usacMembership}</td>
           <td>${coach.isCertified}</td>
         </tr>`))
+      })
     })
-  })
-  // }
+  }
+
+  if (document.location.href.match(/pending$/)) {
+
+    $.getJSON("/api/coaches/pending").then(data => {
+      console.log("List of coaches", data)
+      let tbody = $('#pendingList tbody')
+
+      data.forEach((coach) => {
+        tbody.append($(`<tr>
+          <td>${coach.lastName}</td>
+          <td>${coach.firstName}</td>
+          <td>${coach.teamName}</td>
+          <td>${coach.cprExpDate}</td>
+          <td>${coach.faExpDate}</td>
+          <td>${coach.ssExpDate}</td>
+          <td>${coach.usacMembership}</td>
+          <td>${coach.isCertified}</td>
+        </tr>`))
+      })
+    })
+  }
 
 
 
--- public/javascripts/coaches.js
+++ public/javascripts/coaches.js
@@ -3,24 +3,6 @@ $(document).ready(() => {
 
 
 
-  $.getJSON("/api/coaches/pending").then(data => {
-    console.log("List of coaches", data)
-    let tbody = $('#certifiedList tbody')
-
-    data.forEach((coach) => {
-      tbody.append($(`<tr>
-          <td>${coach.lastName}</td>
-          <td>${coach.firstName}</td>
-          <td>${coach.teamName}</td>
-          <td>${coach.cprExpDate}</td>
-          <td>${coach.faExpDate}</td>
-          <td>${coach.ssExpDate}</td>
-          <td>${coach.usacMembership}</td>
-          <td>${coach.isCertified}</td>
-        </tr>`))
-    })
-  })
-
 
 
 })
