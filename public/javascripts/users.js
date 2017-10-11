b6f50ff35991e3ff233298d7b32be7209aedc047
Brianna Greene <brianna.greene.designs@gmail.com>
2017-10-11 17:38:06 -0600

    updates

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -3,6 +3,21 @@ $(document).ready(() => {
   $('#reg-newcoach-button').click((e) => {
     e.preventDefault()
     console.log('button clicked');
+
+
+        let data = $('#newUserForm').serialize()
+
+        console.log("this is the newUser data: ", data)
+        $.post("/api/coaches", data, null, 'json').then((data) => {
+          console.log("POSTED data", data);
+          document.location = 'coach/home';
+        }).fail((err) => {
+          console.error("THERE WAS AN ERROR WITH THE AJAX POST")
+        })
+
+
+
+
   })
 
 // end of doc ready call
