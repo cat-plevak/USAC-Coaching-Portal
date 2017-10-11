b23f832e2b1929fa52854a05b35face88e2624f5
Brianna Greene <brianna.greene.designs@gmail.com>
2017-10-11 17:33:21 -0600

    working on users.js

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -1,19 +1,29 @@
 $(document).ready(() => {
-  console.log('document ready!')
 
-  $('#newUserForm').submit((e) => {
+  $('#reg-newcoach-button').click((e) => {
     e.preventDefault()
-
-    let data = $('#newUserForm').serialize()
-
-    console.log("this is the newUser data: ", data)
-    $.post("/api/coaches", data, null, 'json').then((data) => {
-      console.log("POSTED data", data);
-      document.location = 'coach/home';
-    }).fail((err) => {
-      console.error("THERE WAS AN ERROR WITH THE AJAX POST")
-    })
+    console.log('button clicked');
   })
 
-
+// end of doc ready call
 })
+
+// $(document).ready(() => {
+//   console.log('document ready!')
+//
+//   $('#newUserForm').submit((e) => {
+//     e.preventDefault()
+//
+//     let data = $('#newUserForm').serialize()
+//
+//     console.log("this is the newUser data: ", data)
+//     $.post("/api/coaches", data, null, 'json').then((data) => {
+//       console.log("POSTED data", data);
+//       document.location = 'coach/home';
+//     }).fail((err) => {
+//       console.error("THERE WAS AN ERROR WITH THE AJAX POST")
+//     })
+//   })
+//
+//
+// })
