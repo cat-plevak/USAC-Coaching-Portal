238bedc49e16968124786f1bbff0f0f3a24590d1
cat-plevak <catherine.plevak@colorado.edu>
2017-10-11 06:45:09 -0600

    adding admin

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -85,4 +85,18 @@ $(document).ready(() => {
     })
   }
 
+  $('#newAdminForm').submit((e) => {
+    e.preventDefault()
+
+    let data = $('#newAdminForm').serialize()
+
+    console.log("this is the newAdmin data: ", data)
+    $.post("/api/admin", data, null, 'json').then((data) => {
+      console.log("POSTED data", data);
+      document.location = '/admins';
+    }).fail((err) => {
+      console.error("THERE WAS AN ERROR WITH THE AJAX POST")
+    })
+  })
+
 })
